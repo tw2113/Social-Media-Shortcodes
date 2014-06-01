e22b41f85ab8fe8a3c3b49b8903aa517e675e015
Michael Beckwith <tw2113@gmail.com>
2014-05-31 19:36:39 -0500

    OOP!

--- social_media_shortcode_plugin.php
+++ social_media_shortcode_plugin.php
@@ -8,50 +8,116 @@ Author: Michael Beckwith
 Author URI: http://michaelbox.net
 */
 
-$smsc_shortcodes = array(
-    'identica' => array( 'Identica', 'http://identi.ca' ),
-    'facebook' => array( 'Facebook', 'http://www.facebook.com' ),
-    'twitter' => array( 'Twitter', 'http://twitter.com' ),
-    'reddit' => array('Reddit', 'http://www.reddit.com/user'),
-    'linkedin' => array('Linkedin', 'http://www.linkedin.com/in'),
-    'stumbleupon' => array('StumbleUpon', 'http://www.stumbleupon.com/stumbler'),
-    'myspace' => array('Myspace', 'http://www.myspace.com'),
-    'digg' => array('Digg', 'http://www.digg.com'),
-    'foursquare' => array('Foursquare', 'http://foursquare.com'),
-    'delicious' => array('Delicious', 'http://www.delicious.com'),
-    'youtube' => array('Youtube', 'http://www.youtube.com'),
-    'gowalla' => array('Gowalla', 'http://www.gowalla.com/users'),
-    'flickr' => array('Flickr', 'http://www.flickr.com/photos'),
-    'vimeo' => array('Vimeo', 'http://www.vimeo.com'),
-    'stickam' => array('Stickam', 'http://www.stickam.com'),
-    'scribd' => array('Scribd', 'http://www.scribd.com'),
-    'slideshare' => array('Slideshare', 'http://www.slideshare.net'),
-    'dailybooth' => array('Daily Booth', 'http://www.dailybooth.com'),
-    'codesnippit' => array('Codesnipp.it', 'http://www.codesnipp.it'),
-    'forrst' => array('Forrst', 'http://www.forrst.com/people'),
-    'dribbble' => array('Dribbble', 'http://www.dribbble.com'),
-    'lastfm' => array('Last.FM', 'http://www.last.fm/user'),
-    'favstarfm' => array('Favstar.FM', 'http://www.favstar.fm/users'),
-    'okcupid' => array('OkCupid', 'http://www.okcupid.com/profile'),
-    'github' => array('Github', 'https://github.com'),
-    'programmableweb' => array('Programmable Web', 'http://www.programmableweb.com/profile'),
-    'colourlovers' => array('Colourlovers', 'http://www.colourlovers.com/lover')
-);
-     
-foreach( array_keys( $smsc_shortcodes ) as $shortcode )
-	add_shortcode( $shortcode, 'smsc_shortcode_handler' );
-     
-    function smsc_shortcode_handler( $atts, $enclosed, $shortcode ) {
-            extract(shortcode_atts( array(
-                    'name' => '',
-                    'text' => '',
-            ), $atts));
-     
-            global $smsc_shortcodes;
-            $service = $smsc_shortcodes[$shortcode][0];
-            $link = $smsc_shortcodes[$shortcode][1];
-     
-            if ( empty($text) ) $text = "$name ($service)";
-     
-    return "<a href=\"$link/$name\" title=\"$name's $service profile\">$text</a>";
-    }
+class Social_Media_Shortcodes {
+
+    public $default_sites       = array();
+    public $all_sites           = array();
+
+	public function __construct() {
+		$this->set_default_sites();
+
+		$this->set_all_sites();
+
+		$this->register_shortcodes();
+	}
+
+	public function set_default_sites() {
+		$this->default_sites = array(
+			'colourlovers'      => array( 'Colourlovers', 'http://www.colourlovers.com/lover' ),
+			'delicious'         => array( 'Delicious', 'http://www.delicious.com' ),
+			'digg'              => array( 'Digg', 'http://www.digg.com' ),
+			'dribbble'          => array( 'Dribbble', 'http://www.dribbble.com' ),
+			'facebook'          => array( 'Facebook', 'http://www.facebook.com' ),
+			'favstarfm'         => array( 'Favstar.FM', 'http://www.favstar.fm/users' ),
+			'flickr'            => array( 'Flickr', 'http://www.flickr.com/photos' ),
+			'forrst'            => array( 'Forrst', 'http://www.forrst.com/people' ),
+			'foursquare'        => array( 'Foursquare', 'http://foursquare.com' ),
+			'github'            => array( 'GitHub', 'https://github.com' ),
+			'lastfm'            => array( 'Last.FM', 'http://www.last.fm/user' ),
+			'linkedin'          => array( 'LinkedIn', 'http://www.linkedin.com/in' ),
+			'myspace'           => array( 'Myspace', 'http://www.myspace.com' ),
+			'okcupid'           => array( 'OkCupid', 'http://www.okcupid.com/profile' ),
+			'programmableweb'   => array( 'Programmable Web', 'http://www.programmableweb.com/profile' ),
+			'reddit'            => array( 'Reddit', 'http://www.reddit.com/user' ),
+			'scribd'            => array( 'Scribd', 'http://www.scribd.com' ),
+			'slideshare'        => array( 'SlideShare', 'http://www.slideshare.net' ),
+			'stumbleupon'       => array( 'StumbleUpon', 'http://www.stumbleupon.com/stumbler' ),
+			'twitter'           => array( 'Twitter', 'http://twitter.com' ),
+			'vimeo'             => array( 'Vimeo', 'http://www.vimeo.com' ),
+			'youtube'           => array( 'YouTube', 'http://www.youtube.com' ),
+		);
+	}
+
+	public function set_all_sites() {
+		$defaults = $this->default_sites;
+		$this->all_sites = apply_filters( 'smsc_shortcodes', $defaults );
+	}
+
+	public function register_shortcodes() {
+		$sites = $this->all_sites;
+
+		//wp_die(print_r($sites));
+
+		foreach( array_keys( $sites ) as $shortcode ) {
+			add_shortcode( $shortcode, array( $this, 'shortcode_handler' ) );
+		}
+	}
+
+	public function get_link_target( $target = '' ) {
+		return ( in_array( $target, $this->get_available_link_targets() ) ) ? $target : '_self';
+	}
+
+	public function get_available_link_targets() {
+		return array( '_self', '_blank', '_parent', '_top' );
+	}
+
+	public function get_classes( $service ) {
+		$classes = apply_filters( 'smsc_classes',
+			array(
+				'smsc',
+				strtolower( $service ) . '_smsc'
+			),
+			$service
+		);
+
+		return implode( ' ', $classes );
+	}
+
+	public function shortcode_handler( $atts, $enclosed, $shortcode ) {
+		$args = shortcode_atts(
+			array(
+				'name'          => '',
+				'text'          => '',
+				'target'        => '_self',
+			),
+			$atts
+		);
+
+
+		$service = $this->all_sites[ $shortcode ][0];
+		$link = $this->all_sites[ $shortcode ][1];
+		$classes = $this->get_classes( $shortcode );
+		$target = $this->get_link_target( $args['target'] );
+
+		if ( empty( $args['name'] ) ) {
+			return sprintf( __( 'You forgot a username for the %s shortcode', 'smsc' ), $service );
+		}
+
+		$text = ( !empty( $args['text'] ) ) ? $args['text'] : $args['name'] . ' (' . $service . ')';
+
+		$output = sprintf( '<a href="%s" title="%s" class="%s" target="%s">%s</a>',
+			trailingslashit( $link ) . $args['name'],
+			$args['name'] .'\'s ' . $service . ' profile',
+			$classes,
+			$target,
+			$text
+		);
+
+		return apply_filters( 'smsc_final_link', $output, $shortcode );
+	}
+}
+
+function smsc_init() {
+	$get_social = new Social_Media_Shortcodes();
+}
+add_action( 'init', 'smsc_init' );
